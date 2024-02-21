2b3ecc6726c301299f9f13df254060138d5eae70
Muhammad Rizky Rizaldi <myquinn489@gmail.com>
2024-02-21 14:21:55 +0800

    Update antrian.php

--- webapps/antrian.php
+++ webapps/antrian.php
@@ -30,7 +30,6 @@ $jam = date("H:i");
 </head>
 
 <body>
-
     <div align="left">
         <script type="text/javascript">
             AC_AX_RunContent('width', '32', 'height', '32'); //end AC code
@@ -79,52 +78,7 @@ $jam = date("H:i");
                                 <img width='180' height='130' src='header-kanan.jpg'/>
                         </td>                                                          
                  </tr>
-          </table> "; 
-    ?>
-    <table width='100%' bgcolor='FFFFFF' border='0' align='center' cellpadding='0' cellspacing='0'>
-         <tr class='head5'>
-           <td width='100%'><div align='center'></div></td>
-         </tr>
-    </table>
-    <table border='0' witdh='100%' cellpadding='0' cellspacing='0'>
-        <tr class='head2' border='0'>
-            <td width='35%' align='center'><font size='6' color='#DD0000'><b>Panggilan Poli</b></font></td><td><font size='6' color='#DD0000'><b>:</b></font></td>
-            <td width='64%' align='center'>
-            <?php 
-                $_sql="select * from antripoli where antripoli.kd_poli='".$kd_poli."' and antripoli.kd_dokter='".$kd_dokter."'" ;  
-                $hasil=bukaquery($_sql);
-                while ($data = mysqli_fetch_array ($hasil)){
-                    echo "<font size='6' color='#DD0000'><b>".getOne("select concat(reg_periksa.no_reg,' ',reg_periksa.no_rawat,' ',pasien.nm_pasien) from reg_periksa inner join pasien on reg_periksa.no_rkm_medis=pasien.no_rkm_medis where reg_periksa.no_rawat='".$data['no_rawat']."'")."</b></font>";
-                    if($data['status']=="1"){
-                        echo "<audio autoplay='true' src='bell.wav'>";
-                        bukaquery2("update antripoli set antripoli.status='0' where antripoli.kd_poli='".$kd_poli."' and antripoli.kd_dokter='".$kd_dokter."'");
-                    }   
-                }
-            ?>
-            </td>
-        </tr>
-        </tr>
-    </table>    
-    <table width='100%' bgcolor='FFFFFF' border='0' align='center' cellpadding='0' cellspacing='0'>
-        <tr class='head4'>
-          <td width='10%'><div align='center'><font size='5'><b>NO</b></font></div></td>
-          <td width='25%'><div align='center'><font size='5'><b>NO.RAWAT</b></font></div></td>
-          <td width='65%'><div align='center'><font size='5'><b>NAMA PASIEN</b></font></div></td>
-        </tr>
-        <?php  
-                $_sql="select reg_periksa.no_reg,reg_periksa.no_rawat,pasien.nm_pasien 
-                       from reg_periksa inner join pasien on reg_periksa.no_rkm_medis=pasien.no_rkm_medis
-                       where reg_periksa.kd_poli='".$kd_poli."' and reg_periksa.kd_dokter='".$kd_dokter."' 
-                       and reg_periksa.tgl_registrasi='".date("Y-m-d", $tanggal)."' and stts='Belum' order by reg_periksa.no_reg" ;  
-                $hasil=bukaquery($_sql);
-
-                while ($data = mysqli_fetch_array ($hasil)){
-                        echo "<tr class='isi7' >
-                                <td align='center'><font size='5' color='#555555' face='Tahoma'>".$data['no_reg']."</font></td>
-                                <td align='center'><font color='#555555' size='5'  face='Tahoma'>".$data['no_rawat']."</font></td>
-                                <td align='center'><font color='#555555' size='5'  face='Tahoma'>".$data['nm_pasien']."</font></td>
-                            </tr> ";
-                }
+          </table> ";
         ?>
         <table width='100%' bgcolor='FFFFFF' border='0' align='center' cellpadding='0' cellspacing='0'>
             <tr class='head5'>
@@ -203,9 +157,9 @@ $jam = date("H:i");
 
             while ($data = mysqli_fetch_array($hasil)) {
                 echo "<tr class='isi7' >
-                                <td align='center'><font size='5' color='gray' face='Tahoma'>" . $data['no_reg'] . "</font></td>
-                                <td align='center'><font color='#DDDD00' size='5'  face='Tahoma'>" . $data['no_rawat'] . "</font></td>
-                                <td align='center'><font color='gren' size='5'  face='Tahoma'>" . $data['nm_pasien'] . "</font></td>
+                                <td align='center'><font size='5' color='#000000' face='Tahoma'>" . $data['no_reg'] . "</font></td>
+                                <td align='center'><font color='#000000' size='5'  face='Tahoma'>" . $data['no_rawat'] . "</font></td>
+                                <td align='center'><font color='#000000' size='5'  face='Tahoma'>" . $data['nm_pasien'] . "</font></td>
                             </tr> ";
             }
             ?>
@@ -219,6 +173,7 @@ $jam = date("H:i");
         </table>
         <img src="ft-2.jpg" alt="bar-pic" width="100%" height="83">
         <script src="https://code.responsivevoice.org/responsivevoice.js?key=OGPOBj1g"></script>
+    </div>
 </body>
 <?php
 echo "<meta http-equiv='refresh' content='20;URL=?iyem=" . encrypt_decrypt("{\"kd_poli\":\"" . $kd_poli . "\",\"kd_dokter\":\"" . $kd_dokter . "\"}", "e") . "'>";
